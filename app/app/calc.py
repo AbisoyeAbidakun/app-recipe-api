ce85423baa28000db8a6dbe78291d2fb447e1403
Abidakun Abisoye <abidakunabisoye@Abidakuns-MacBook-Pro.local>
2023-07-11 10:52:22 +0100

    fixed linted error

--- app/app/calc.py
+++ app/app/calc.py
@@ -7,11 +7,9 @@ def add(x, y):
 
     """ Add x and y and return results. """
 
-    return x + y 
-
+    return x + y
 
 def subtract(x, y):
-    
     """ Subtract x and y and return results. """
 
-    return y - x  
+    return y - x
